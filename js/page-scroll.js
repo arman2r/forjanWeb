d67316e321b5a2d35df68418fdcffbab391534ea
armando.rubio <armando.rubio@servientrega.com>
2023-07-26 23:36:21 -0500

    mejoras responsive

--- js/page-scroll.js
+++ js/page-scroll.js
@@ -9,21 +9,4 @@ function scrollFunction() {
     document.getElementById('container-page').classList.remove('resize-mask')
   }
 }
-
  
-/*
-let scrollpos = window.scrollY
-const header = document.getElementById("header")
-const header_height = header.offsetHeight
-
-const add_class_on_scroll = () => header.classList.add("smaller")
-const remove_class_on_scroll = () => header.classList.remove("smaller")
-
-document.getElementById('container-page').addEventListener('scroll', function() { 
-  scrollpos = window.scrollY;
-
-  if (scrollpos >= header_height) { add_class_on_scroll() }
-  else { remove_class_on_scroll() }
-
-  console.log(scrollpos)
-})*/
